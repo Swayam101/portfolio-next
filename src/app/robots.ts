1f9e1cf777ac9746aec96cb13ca7a02e04565030
Swayam101 <swayamprajapat21@gmail.com>
2026-09-10 01:17:54 +0530

    modified robots txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/admin/"],
+        disallow: ["/admin/"],
       },
     ],
     sitemap: "https://www.swayam.cyou/sitemap.xml",
